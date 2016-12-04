eb311e0c72f7535cca66fad6946024e8d88cf7f9
Liam DeBeasi <liamdebeasi@icloud.com>
2016-12-03 20:56:31 -0500

    ugh

--- app/index.js
+++ app/index.js
@@ -551,7 +551,7 @@ function createPlaylist(userID, accessToken, title, collaborative, trackListings
                         var externalURL = playlistBody.external_urls.spotify;
                         for (var n in numbers) {
                             tw.messages.create({
-                                body: "Hi there, you have been invited to collaborate on the " + title + " playlist on Spotify. Tap the following link to get started: " + externalURL,
+                                body: 'Hi there, you have been invited to collaborate on the "' + title + '" playlist on Spotify. Tap the following link to get started: ' + externalURL,
                                 to: numbers[n],
                                 from: "7742785522"
                             }, function(err, data) {
@@ -624,33 +624,6 @@ function createPlaylist(userID, accessToken, title, collaborative, trackListings
         }                                                   
     }); 
 }
-
-/*
-
-app.get('/refresh_token', function(req, res) {
-
-  // requesting access token from refresh token
-  var refresh_token = req.query.refresh_token;
-  var authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
-    form: {
-      grant_type: 'refresh_token',
-      refresh_token: refresh_token
-    },
-    json: true
-  };
-
-  request.post(authOptions, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
-      var access_token = body.access_token;
-      res.send({
-        'access_token': access_token
-      });
-    }
-  });
-});
-*/
 
 console.log('Listening on 8888');
 app.listen(8888);
